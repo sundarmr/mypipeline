85c19ebaa35ef4ce1a359b880c2dcc8ea95a4aec
Sundar Rajendran <sundar.moorthy@gmail.com>
2018-04-26 21:47:35 -0400

    Update ExpirationDateValidatorProcessor.java

--- src/main/java/com/example/pipeline/ExpirationDateValidatorProcessor.java
+++ src/main/java/com/example/pipeline/ExpirationDateValidatorProcessor.java
@@ -5,6 +5,10 @@ public class ExpirationDateValidatorProcessor extends ValidationProcessor {
 	public boolean validate(CommunicationRequest request) {
 		System.out.println("In "+ExpirationDateValidatorProcessor.class.getCanonicalName());
 		//super.cancelCommunicationRequest(request);
+		if(request.getRequestMetaData().isDoesExpire()) {
+			//process
+			
+		}
 		return true;
 	}
 
